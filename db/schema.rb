a966bb56f835ed77eaa542c8815b98e1310417dc
Avijit Karmaker <ajkacca@gmail.com>
2020-11-26 15:28:58 +0200

    group scaffold add

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_26_123923) do
+ActiveRecord::Schema.define(version: 2020_11_26_132740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "groups", force: :cascade do |t|
+    t.string "title"
+    t.string "icon"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "transactions", force: :cascade do |t|
     t.string "title"
@@ -21,6 +28,8 @@ ActiveRecord::Schema.define(version: 2020_11_26_123923) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "group_id"
+    t.index ["group_id"], name: "index_transactions_on_group_id"
     t.index ["user_id"], name: "index_transactions_on_user_id"
   end
 
@@ -37,5 +46,6 @@ ActiveRecord::Schema.define(version: 2020_11_26_123923) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "transactions", "groups"
   add_foreign_key "transactions", "users"
 end
